d4c25f5599c3fb13d741912151d0ef7670c0e996
SLKBunny <WulfyB@gmail.com>
2017-10-22 17:46:43 -0500

    Still trash, but more logic added

--- UDPServer.cpp
+++ UDPServer.cpp
@@ -46,13 +46,21 @@ int main(void)
 	char buf[MAXBUFLEN];
 	socklen_t addr_len;
 	char s[INET6_ADDRSTRLEN];
-
+	int hasError = 0;
+	int lengthError = 0;
+	int magicError = 0;
+	int checkSumError = 0;
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC; // set to AF_INET to force IPv4
 	hints.ai_socktype = SOCK_DGRAM;
 	hints.ai_flags = AI_PASSIVE; // use my IP
 
-	while (1) {
+	while (1)
+	{
+		hasError = 0;
+		checkSumError = 0;
+		lengthError = 0;
+		magicError = 0;
 		if ((rv = getaddrinfo(NULL, MYPORT, &hints, &servinfo)) != 0) {
 			fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 			return 1;
@@ -90,15 +98,12 @@ int main(void)
 			perror("recvfrom");
 			exit(1);
 		}
-		
+
 		printf("listener: got packet from %s\n",
 			inet_ntop(their_addr.ss_family,
 				get_in_addr((struct sockaddr *)&their_addr),
 				s, sizeof s));
-		
-		//CALCULATE CHECKSUM
 
-		//Process the incoming bytes
 
 		unsigned long recMagicNumber = buf[0] << 24 | buf[1] << 16 | buf[2] << 8 | buf[3];
 		recMagicNumber = ntohl(recMagicNumber);
@@ -106,67 +111,133 @@ int main(void)
 		recTML = noths(recTML);
 		if (numbytes != recTML)
 		{
-			//construct fail message
+			lengthError = 1;
 		}
-		unsigned char checksum = 0;
-		unsigned short bytesum = 0;
+		unsigned short byteSum;
 		unsigned int count = 0;
 		for (short i = 0; i < numbytes; i++)
 		{
-			if (i = 5)
+			if (i = 7)
 			{
 				continue;
 			}
-			bytesum += buf[i];
+			byteSum += buf[i];
 		}
-		checksum = bytesum; //Obviously losing data at the moment, need updated to double
-		//check method of finding checksum
-		//while ((bytesum & 0xff00) > 0) 
-		//{
-		//		bytesum += 0x1;
-		//	}
-		if (checksum != buf[5])
-		{
+		unsigned char checksum = getCheckSum(byteSum); //Obviously losing data at the moment, need updated to double
 
+		if (checksum != buf[7])
+		{
+			hasError = 1;
+			checkSumError = 1;
 		}
 		unsigned char recGID = buf[6];
 		unsigned char recCSum = buf[7];
 		unsigned char requestID = buf[8];
 		unsigned int numOfHosts = 0;
 		unsigned long magicNumber = 0x4A6F7921;
-		
-		if (magicNumber == recMagicNumber) {
-			for (int i = 0; i < recTML; i++)
-			{
-				unsigned short hostLength = buf[i];
-				std::string hostName = "";
-				unsigned long hostIP;
-				for (int j = 0; j < hostLength; j++)
-				{
-					hostName += buf[i + j];
 
-				}
-				numOfHosts++;
-				//hostname should be constructed
-				//Need to fetch the ip address and add to return message
-				hostIP = fetchHostIP(hostName);
+		if (magicNumber == recMagicNumber)
+		{
+			hasError = 1;
+			magicError = 1;
+		}
+		if (hasError)
+		{
+			char errBuf[9];
+			magicNumber = htonl(magicNumber);
+			errBuf[0] = magicNumber << 24;
+			errBuf[1] = magicNumber << 16;
+			errBuf[2] = magicNumber << 8;
+			errBuf[3] = magicNumber;
+			short TML = 9;
+			TML = htons(TML);
+			errBuf[4] = TML << 8;
+			errBuf[5] = TML;
+			errBuf[6] = 0;
+			errBuf[7] = 13; //hardcoded, group ID is 13
+			if (lengthError)
+			{
+				errBuf[8] = errBuf[8] | 0x01; //error code, b0 should be 1 indicating length mismatch.
 			}
+			if (checkSumError)
+			{
+				errBuf[8] = errBuf[8] | 0x02; //error code, b1 should be 1 indicating checksum is incorrect.
+			}
+			if (magicError)
+			{
+				errBuf[8] = errBuf[8] | 0x04; //error code, b0 should be 1 indicating no or wrong magic number.
+			}
+			//errBuf[8] should now be all errors from received message
+			for (short i = 0; i < 9; i++)
+			{
+				byteSum += errBuf[i];
+			}
+			errBuf[6] = getCheckSum(byteSum);
+			if (sendto(sockfd, errBuf, TML, 0, (struct sockaddr *)&their_addr, sizeof(their_addr)) !=
+				TML)
+			{
+				perror("Failed to send error for invalid message; resuming listening");
+			}
+			continue; //Moves the program back to start of while loop to wait for next message.
+		}
+		std::string hostIP[256];
+		for (int i = 0; i < recTML; i++)
+		{
+			unsigned short hostLength = buf[i];
+			std::string hostName = "";
+			for (int j = 0; j < hostLength; j++)
+			{
+				hostName += buf[i + j];
+
+			}
+			numOfHosts++;
+			//hostname should be constructed
+			//Need to fetch the ip address and add to return message
+			hostIP[i] = fetchHostIP(hostName);
 
 		}
-		else //magicNumber error!
+		//Host IP Addresses should now be stored in the hostIP array. Now need to put into buffer
+
+
+		char message[MAXBUFLEN];
+		numOfHosts *= 4; //each host takes 4 bytes
+		unsigned short TML = 9 + numOfHosts;
+		magicNumber = htonl(magicNumber);
+		buf[0] = magicNumber << 24;
+		buf[1] = magicNumber << 16;
+		buf[2] = magicNumber << 8;
+		buf[3] = magicNumber;
+		numOfHosts /= 4;
+		buf[4] = TML << 8;
+		buf[5] = TML;
+		buf[6] = 13; //Hardcoded
+		buf[7] = 0;
+		buf[8] = requestID;
+		int hostTracker = 0;
+		for (int i = 9; 0 < numOfHosts; i++)
+		{
+			std::string host = hostIP[hostTracker];
+			unsigned long hostnum = std::stoi(host);
+			hostnum = htonl(host);
+			buf[i] = host<< 24;
+			i++;
+			buf[i] = host<< 16;
+			i++;
+			buf[i] = host << 8;
+			i++;
+			buf[i] = host;
+			numOfHosts--;
+		}
+
+		//calculate checksum
+		for (short i = 0; i < TML; i++)
 		{
 
+			byteSum += buf[i];
 		}
-		numOfHosts * 4; //each host takes 4 bytes
-		unsigned short TML = 9 + numOfHosts;
-
-		buf[0] = TML;
-		buf[1] = requestID;
-
-		//Add response back to buffer
-
-		if (sendto(sockfd, buf, strlen(buf), 0, (struct sockaddr *)&their_addr, sizeof(their_addr)) !=
-			strlen(buf))
+		buf[7] = getCheckSum(byteSum);
+		if (sendto(sockfd, buf, TML, 0, (struct sockaddr *)&their_addr, sizeof(their_addr)) !=
+			TML)
 		{
 			perror("Failed to echo");
 		}
@@ -175,8 +246,21 @@ int main(void)
 	}//END OF BIG WHILE
 	return 0;
 }
-unsigned long fetchHostIP(std::string hostName)
+std::string fetchHostIP(std::string hostName)
 {
-	
-	return 0;
+
+	return ""; //maybe find int?
+}
+
+unsigned char getCheckSum(short byteSum)
+{
+	unsigned char checksum = 0;
+	while ((byteSum & 0xFF00) > 0) {
+		unsigned char leftHalf = (char)(byteSum >> 8);
+		unsigned char rightHalf = (char)byteSum;
+		byteSum = (short)(leftHalf + rightHalf);
+
+	}
+	checksum = byteSum;
+	return checksum;
 }
